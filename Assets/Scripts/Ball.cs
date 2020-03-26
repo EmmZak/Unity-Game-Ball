672c4b8342f6d40b1535dd63984220afded43b3a
Emmanuel <emmanuelzakaryan@gmail.com>
2020-03-26 23:16:00 +0100

    added colors to objects, jumps on touch
    workds fine

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rb;
+    int speed = 20;
+    float bx;
+    float by;
+    float bz;
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+/*
+        bx = Input.acceleration.x;
+        by = Input.acceleration.y;
+*/
+        bx = 0;
+        by = 0;
+        bz = 0;
+        movement = new Vector3(bx, 0.0f, by);
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        bx = Input.acceleration.x;
+        
+        by = by + 0.005f;
+
+        movement = new Vector3(bx * speed, 0.0f, by);
+        rb.AddForce(movement);
+
+    }
+
+    void FixedUpdate()
+    {
+        movement = new Vector3(Input.acceleration.x, 0, -Input.acceleration.z);
+        if (Input.GetMouseButtonDown(0))
+        {
+            //jump here
+            rb.velocity = new Vector3(rb.velocity.x, 5, rb.velocity.z);
+        }
+
+    }
+
+    public void Jump()
+    {
+        bz = bz + 0.5f;
+    }
+}
